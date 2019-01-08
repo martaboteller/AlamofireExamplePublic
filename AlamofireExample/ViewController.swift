b7dac7dba874ecac8301880fe8e989bf4cb538e0
Marta Boteller <marta.boteller@gmail.com>
2019-01-08 20:15:28 +0100

    Initial Commit

---
+++ AlamofireExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireExample
+//
+//  Created by Marta Boteller on 08/01/2019.
+//  Copyright © 2019 Marta Boteller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
